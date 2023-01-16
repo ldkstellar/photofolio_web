3c75545e10ba52a668f213cc26f86d03d010ae18
ldk-stellar <ldkstellar@github.com>
2023-01-16 19:52:10 +0900

    projects

---
+++ projects/homework_1/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.gxzsir3.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {'name': name_receive, 'comment':comment_receive}
+    print(doc)
+    db.mini.insert_one(doc)
+
+    return jsonify({'msg':'POST 연결 완료!'})
+
+
+
+
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    all_mini = list(db.mini.find({}, {'_id': False}))
+    return jsonify({'mini':all_mini})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
